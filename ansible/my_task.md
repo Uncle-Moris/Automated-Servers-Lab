add9923b349979f4769f70a2d50086e1cb995000
Uncle_Moris <mjw11@wp.pl>
2024-07-08 16:34:45 +0200

    add ansible playbok for ngnix

--- ansible/my_task.md
+++
@@ -1,3 +0,0 @@
-1. Prepere bootstraping for ansible user 
-2. Set up nginx on Centos & Ubuntu web_servers, serv staticsite
-3. Set up PostgreSQL on 
--- ansible_LL/set_up_ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ssh-keygen -C "ansible" -f ~/.ssh/ansible -N "" &&
-
-ssh-copy-id -i /home/vagrant/.ssh/ansible.pub node1 &&
-ssh-copy-id -i /home/vagrant/.ssh/ansible.pub node2 &&
-ssh-copy-id -i /home/vagrant/.ssh/ansible.pub node8
